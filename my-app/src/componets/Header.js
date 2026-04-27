adfd09d966eeb11d1915558d6b0843acab37ccfd
lalitraghavraj <lalitraghavraj@gmail.com>
2026-04-27 05:55:21 +0530

    header change

--- my-app/src/componets/Header.js
+++ my-app/src/componets/Header.js
@@ -4,7 +4,7 @@ import logo from "../assets/logo.jpg"
 export const Header=()=>{
 return(
 <div>
-    <h1>Header</h1>
+    <h1>Header this is header</h1>
 <img className="App-logo"  src={logo} />
 
 </div>
