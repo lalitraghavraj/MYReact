5a0ab7468f68afb56586f6070cb5aee9fa212b24
lalitraghavraj <lalitraghavraj@gmail.com>
2026-04-28 05:49:38 +0530

    add css

--- my-app/src/componets/Header.js
+++ my-app/src/componets/Header.js
@@ -1,12 +1,12 @@
 import logo from "../assets/logo.jpg"
-
+import "./header.css"
 
 export const Header=()=>{
 return(
 <div>
     <h1>Header this is header</h1>
      <h1>Header this is header</h1>
-<img className="App-logo"  src={logo} />
+<img className="logo"  src={logo} />
 
 </div>
 
